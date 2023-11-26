9c6d6fea21cb2e4859c388f1980e30441fb042a0
MythicXGN <hydythediny@gmail.com>
2023-11-26 14:02:51 -0700

    Player fixed

--- src/commands/music/play.ts
+++ src/commands/music/play.ts
@@ -134,7 +134,7 @@ export default class PlayCommand extends Command {
             
             if(previousPage){
                 const previousEmbed = this.createEmbed(interaction, previousPage);
-                interaction.editOriginal({
+                interaction.createMessage({
                     embeds: [previousEmbed],
                     components: this.components
                 });
